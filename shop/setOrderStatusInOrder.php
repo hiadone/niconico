1ad775a454acc77d00e0f8ba6c57f67a722ec05a
naboyoon <boyoon8@gmail.com>
2021-05-10 18:42:28 +0900

    b

--- shop/setOrderStatusInOrder.php
+++ shop/setOrderStatusInOrder.php
@@ -8,7 +8,7 @@ if ($default['de_autoCompleteShipDay'] > 0) {
 	$sql = " SELECT * FROM {$g5['g5_shop_order_table']} WHERE od_status = '배송' ";
 	$result = sql_query($sql);
     for ($i=0; $row=sql_fetch_array($result); $i++) {
-		if (date("YmdHi", strtotime($row['od_invoice_time'])) == date("YmdHi", time() - ($default['de_autoCompleteShipDay'] * 24 * 60 * 60))) {
+		if (date("YmdHi", strtotime($row['od_invoice_time'])) < date("YmdHi", time() - ($default['de_autoCompleteShipDay'] * 24 * 60 * 60))) {
 			$uSql = " UPDATE {$g5['g5_shop_order_table']} SET od_status = '완료' WHERE od_id = '". $row['od_id'] ."' ";
 			sql_query($uSql);
 		}
